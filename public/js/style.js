26cf1042f0ccb579fe7d1ce39399ecbdcb42d965
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-08-08 23:15:22 +0100

    created side-bar

--- public/js/style.js
+++ public/js/style.js
@@ -13,8 +13,8 @@ $navLogin.on('click', () => {
 /** quick options configurations */
 
 //show and hide quick options
-let opt = $('.quick-options');
-let optCount = opt.children().length;
+const opt = $('.quick-options');
+const optCount = opt.children().length;
 
 //quick options icon array
 let $iconElement = ['.quick-search i', '.quick-question i'];
@@ -28,9 +28,11 @@ for(let x in $iconElement){
     let $optionContainer = $($optionsClass[x]);
     $icon.on('click', ()=>{
         if($icon.attr('class') !== 'fas fa-times'){
-            $optionContainer.animate( {'right': 0}, 250);
-            $icon.attr('class', 'fas fa-times');
+            $optionContainer.animate( {'right': '400px'}, 250).addClass('opt-shadow');
             $optionContainer.find('input')[0].focus();
+            console.log($optionContainer);
+            $icon.attr('class', 'fas fa-times');
+
             let sibsP = $($optionsClass[x]).prevAll();
             let sibsN = $($optionsClass[x]).nextAll();
             let index = optCount - sibsN.length;
@@ -38,52 +40,50 @@ for(let x in $iconElement){
             //all previous siblings
             if(sibsP.length > 0){
                 for(let y in sibsP){
-                    if($(sibsP[y]).css('right') !== '-400px'){
-                        $(sibsP[y]).animate({'right': '-400px'}, 250);
+                    if($(sibsP[y]).css('right') !== 0){
+                        $(sibsP[y]).animate({'right': 0}, 250);
                         $($iconElement[y]).attr('class', $iconClass[y]);
+                        $($optionsClass[y]).removeClass('opt-shadow');
                     }
                 }
             }
             //all next siblings
             if(sibsN.length > 0){
                 for( let z in sibsN){
-                    if($(sibsN[z]).css('right') !== '-400px'){
-                        $(sibsN[z]).animate({'right': '-400px'}, 250);
+                    if($(sibsN[z]).css('right') !== 0){
+                        $(sibsN[z]).animate({'right': 0}, 250);
                         $($iconElement[index]).attr('class', $iconClass[index]);
+                        $($optionsClass[index]).removeClass('opt-shadow');
                     }
                     index++;
                 }
             }
         }
         else{
-            $optionContainer.animate( {'right': '-400px'}, 250);
+            $optionContainer.animate( {'right': 0}, 250).removeClass('opt-shadow');
             $icon.attr('class', $iconClass[x]);
         }
     });
-}
+};
 
 /** mobile navigation configuration */
-let $mobIcon = $('.mobile-navigation i');
-let $mobileNav = $('.mobile-navigation');
+const $mobIcon = $('.mobile-navigation i');
+const $mobileNav = $('.mobile-navigation');
 $mobIcon.on('click', ()=>{
     if($mobIcon.attr('class') !== 'fas fa-times' ){
         $mobIcon.next().animate({'right': 0}, 250);
         $mobIcon.attr('class','fas fa-times');
     }
     else{
-<<<<<<< HEAD
-        $mobIcon.next().animate({'right': '-592px'}, 300);
-=======
         $mobIcon.next().animate({'right': '-768px'}, 300);
->>>>>>> UI
         $mobIcon.attr('class','fas fa-bars');
     }
 });
 
 /** mobile search configuration*/
-let $mobSearchIcon =$('.mob-search .fa-search');
-let $searchForm = $('.mob-search form');
-let $searchFormInput = $searchForm.find('input[type="text"]');
+const $mobSearchIcon =$('.mob-search .fa-search');
+const $searchForm = $('.mob-search form');
+const $searchFormInput = $searchForm.find('input[type="text"]');
 $mobSearchIcon.on('click', () => {
   if( $searchForm.css('display') !== 'block'){
     $mobSearchIcon.css('color', 'red');
@@ -99,6 +99,7 @@ $searchFormInput.on('blur', ()=>{
   $searchForm.slideUp();
   $mobSearchIcon.css('color', 'white');
 });
+
 
 
 });
