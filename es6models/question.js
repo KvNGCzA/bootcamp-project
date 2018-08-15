67b459fabbe439c391ea643a19ce71abba951b03
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-08-15 22:15:26 +0100

    [#159808461] added create new question API

---
+++ es6models/question.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+
+export const fetchQuestions = () => {
+    try {
+      let questionDatabase = fs.readFileSync('databases/questions.json');
+      return JSON.parse(questionDatabase);
+    } catch (error) {
+     return [];
+    }
+};
+
+const saveQuestion = questions => {
+    fs.writeFileSync('databases/questions.json',JSON.stringify(questions, undefined, 2));
+};
+
+export const createNewQuestion = (title, content, callback) => {
+  if(!title){
+    return callback({ status: 400, message: 'Title can not be empty'});
+  }
+  else if(!content){
+    return callback({ status: 400,message: 'Content can not be empty' });
+  }
+  const currentQuestions = fetchQuestions();
+  const questionId = `question${currentQuestions.length + 1}`;
+  const newQuestion = {
+    questionId,
+    title: title.trim(),
+    content: content.trim(),
+    answer: []
+  };
+  currentQuestions.push(newQuestion);
+  saveQuestion(currentQuestions);
+  callback(undefined,{status: 201, result: 'Question added'});
+};
