d7c671517210ab9a6301257fdc7629513e61a2ed
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-08-16 17:25:03 +0100

    [#159822663] added get question by Id API test

--- server/server.js
+++ server/server.js
@@ -17,9 +17,6 @@ var _hbs = require('hbs');
 
 var _hbs2 = _interopRequireDefault(_hbs);
 
-var _morgan = require('morgan');
-
-var _morgan2 = _interopRequireDefault(_morgan);
 
 var _questions = require('../api/routes/questions');
 
@@ -32,7 +29,6 @@ var port = process.env.PORT || 3000;
 /**start express server*/
 var app = (0, _express2['default'])();
 
-app.use((0, _morgan2['default'])('dev'));
 app.use(_bodyParser2['default'].urlencoded({ extended: false }));
 app.use(_bodyParser2['default'].json());
 app.use(function (req, res, next) {
--- server/server.test.js
+++ server/server.test.js
@@ -2,7 +2,7 @@ const request= require('supertest');
 const bodyParser = require('body-parser');
 
 const app = require('./server').app;
-
+const question = require('../api/models/question');
 
 describe('GET /api/v1/questions', () => {
   it('should return status 200', (done) => {
@@ -11,7 +11,7 @@ describe('GET /api/v1/questions', () => {
   .expect(200)
   .end(done);
   });
-  it('should return json format', (done) => {
+  it('should return JSON format', (done) => {
   request(app)
   .get('/api/v1/questions')
   .expect('Content-type', /json/)
@@ -20,17 +20,16 @@ describe('GET /api/v1/questions', () => {
 });
 
 describe('POST /api/v1/questions', () => {
-  it('should return json', (done) => {
-  const obj = {
-    title: "test",
-    content: "test"
-  };
+  it('should return JSON format', (done) => {
   request(app)
   .post('/api/v1/questions')
-  .send(obj)
+  .send({
+    title: 'test',
+    content: 'test'
+  })
   .expect(201, {
     status: 201,
-    result: "Question added"
+    result: 'Question added'
   })
   .end(done);
   });
@@ -40,7 +39,7 @@ describe('POST /api/v1/questions', () => {
   .send({yam: 'egg'})
   .expect(400, {
     status: 400,
-    message: "Title can not be empty"
+    message: 'Title can not be empty'
   })
   .end(done);
   });
@@ -50,8 +49,35 @@ describe('POST /api/v1/questions', () => {
   .send({title: 'egg'})
   .expect(400, {
     status: 400,
-    message: "Content can not be empty"
+    message: 'Content can not be empty'
   })
   .end(done);
+  });
+});
+
+const allQuestions = question.fetchQuestions();
+const lastItem = allQuestions.length + 1;
+
+describe('GET /api/v1/question/questionId', () => {
+  it('should return 200 & JSON format', (done) => {
+    request(app)
+    .get(`/api/v1/questions/question${lastItem}`)
+    .expect(200, [{
+      questionId: `question${lastItem}`,
+      title: 'test',
+      content: 'test',
+      answer: []
+    }])
+    .expect('Content-type', /json/)
+    .end(done);
+  });
+  it('should return error for wrong ID', (done) => {
+    request(app)
+    .get('/api/v1/questions/question')
+    .expect(404, {
+    status: 404,
+    message: 'Invalid question id'
+  })
+    .end(done);
   });
 });
