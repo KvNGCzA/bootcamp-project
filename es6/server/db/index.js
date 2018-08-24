9bed4733bcaad383ec81f22d4c817fbc9ed3ed20
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-08-24 13:01:42 +0100

    [#159959129] remade start script

--- es6/server/db/index.js
+++ es6/server/db/index.js
@@ -1,3 +1,5 @@
-require('dotenv').config();
+import dotEnv from 'dotenv';
+
+dotEnv.config();
 const pgp = require('pg-promise')();
 export const db = pgp(process.env.DB);
--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-require('dotenv').config();
+exports.db = undefined;
+
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+_dotenv2['default'].config();
 var pgp = require('pg-promise')();
 var db = exports.db = pgp(process.env.DB);
