7e336bf48dde6789456184d20bb867b33d619c86
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-09-21 05:18:15 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -1,116 +1,134 @@
-/** add page-title as a class to the body tag*/
-const dashTitle = str =>{
-  if( /\s/.test(str) ){
-    let lowerStr = str.toLowerCase();
-    let strArr = lowerStr.split(' ');
-    return strArr.join('-');
-  }
-  return str.toLowerCase();
-};
-  let bodyTag = document.getElementsByTagName('body');
-  let currentPageTitle = document.title;
-  let cName = dashTitle(currentPageTitle);
-  bodyTag[0].classList += "page-" + cName + " " ;
+'use strict';
+
+/** add page-title as a class to the body tag */
+var bodyTag = document.getElementsByTagName('body')[0];
+// const currentPageTitle = document.title;
+// const cName = dashTitle(currentPageTitle);
+// bodyTag.classList += `page-${cName} `;
+
+// add logged-in or logged-out class to body
+var status = localStorage.getItem('token');
+if (status !== 'null') {
+	bodyTag.classList += ' logged-in';
+	var profileLink = document.getElementsByClassName('profile-link');
+	var username = localStorage.getItem('username');
+	var profileimage = localStorage.getItem('profileimage');
+	for (var x = 0; x < profileLink.length; x++) {
+		profileLink[x].setAttribute('href', '/profile?username=' + String(username));
+	}
+	document.getElementsByClassName('profile-image')[0].style.backgroundImage = 'url(\'images/' + String(profileimage) + '\')';
+} else {
+	bodyTag.classList += ' logged-out';
+}
+
+/** action buttons config */
+var actionBtnArr = ['far fa-thumbs-up likebutton', 'far fa-thumbs-down dislikebutton', 'far fa-flag reportbutton', 'far fa-star favoritebutton'];
+var thinClass = ['far fa-thumbs-up likebutton', 'far fa-thumbs-down dislikebutton', 'far fa-flag reportbutton', 'far fa-star favoritebutton'];
+var thickClass = ['fas fa-thumbs-up likebutton', 'fas fa-thumbs-down dislikebutton', 'fas fa-flag reportbutton', 'fas fa-star favoritebutton'];
+
+var _loop = function () {
+	function _loop(_x) {
+		var current = document.getElementsByClassName(actionBtnArr[_x]);
+		for (var _y = 0; _y < current.length; _y++) {
+			current[_y].addEventListener('click', function () {
+				var currentClass = this.getAttribute('class');
+				if (currentClass === thinClass[_x]) {
+					this.setAttribute('class', thickClass[_x]);
+				} else {
+					this.setAttribute('class', thinClass[_x]);
+				}
+			}, false);
+		}
+	}
+
+	return _loop;
+}();
+
+for (var _x in actionBtnArr) {
+	_loop(_x);
+}
+
+/** comments list background color */
+var comments = document.getElementsByClassName('comment-cont');
+for (var _x2 in comments) {
+	if (_x2 % 2 === 0) {
+		comments[_x2].style.backgroundColor = '#f4f4f4';
+	}
+}
+
+/** colour for question meta - views, likes and answered if count is greater than 0 */
+var homeAnswered = document.getElementsByClassName('answer-count-dis');
+var homeLiked = document.getElementsByClassName('likes-count-dis');
+var homeViews = document.getElementsByClassName('views-count-dis');
+
+var countArr = [homeAnswered, homeLiked, homeViews];
+var classCountArr = ['answered', 'liked', 'viewed'];
+for (var y in countArr) {
+	for (var _x3 in countArr[y]) {
+		var _current = Number(countArr[y][_x3].textContent);
+		if (_current > 0) {
+			countArr[y][_x3].classList += ' ' + String(classCountArr[y]);
+		} // if
+	} // for x
+} // for y
 
 
-/** action buttons config*/
-  let actionBtnArr = ['far fa-thumbs-up likebutton','far fa-thumbs-down dislikebutton','far fa-flag reportbutton','far fa-star favoritebutton'];
-  let thinClass = ['far fa-thumbs-up likebutton','far fa-thumbs-down dislikebutton','far fa-flag reportbutton','far fa-star favoritebutton'];
-  let thickClass = ['fas fa-thumbs-up likebutton','fas fa-thumbs-down dislikebutton','fas fa-flag reportbutton','fas fa-star favoritebutton'];
-  for(let x in actionBtnArr ){
-    let current = document.getElementsByClassName(actionBtnArr[x]);
-      for(let y = 0; y < current.length; y++){
-        current[y].addEventListener('click', function(){
-          let currentClass = this.getAttribute('class');
-          if( currentClass === thinClass[x]){
-            this.setAttribute('class', thickClass[x]);
-          }
-          else{
-            this.setAttribute('class',thinClass[x]);
-          }
-        }, false);
-      }
+// mobile search configuration
+var mobSearchIcon = document.getElementById('fa-search');
+var searchForm = document.getElementsByClassName('mob-search-form');
+var searchFormInput = document.getElementsByClassName('search-form-input');
+mobSearchIcon.addEventListener('click', function () {
+	if (searchForm[0].style.display === 'block') {
+		mobSearchIcon.style.color = 'white';
+		searchForm[0].style.display = 'none';
+	} else {
+		mobSearchIcon.style.color = '#f24d4d';
+		searchForm[0].style.display = 'block';
+	}
+}, false);
+searchFormInput[0].addEventListener('blur', function () {
+	searchForm[0].style.display = 'none';
+	mobSearchIcon.style.color = 'white';
+}, false);
 
-  }
+/** functions the display either login or sign up forms */
+var loginCheckArr = ['login-form', 'side-login-form'];
+var signupCheckArr = ['signup-form', 'side-signup-form'];
+var changeButtons = ['c-t-login', 's-t-login'];
+var changeButtons2 = ['c-t-signup', 's-t-signup'];
+var checkWhichForm = function () {
+	function checkWhichForm(login, signup) {
+		if (login.style.display === 'none') {
+			signup.style.display = 'none';
+			login.style.display = 'block';
+		} else {
+			signup.style.display = 'block';
+			login.style.display = 'none';
+		}
+	}
 
+	return checkWhichForm;
+}(); // checkWhichForm
 
-  /** comments list background color */
-  let comments = document.getElementsByClassName('comment-cont');
-  for( let x in comments){
-    if( x % 2 === 0 ){
-      comments[x].style.backgroundColor= '#f4f4f4';
-    }
-  }
+var _loop2 = function () {
+	function _loop2(_x4) {
+		var loginCheck = document.getElementById(loginCheckArr[_x4]);
+		var signupCheck = document.getElementById(signupCheckArr[_x4]);
+		var loginButton = document.getElementById(changeButtons[_x4]);
+		var signupButton = document.getElementById(changeButtons2[_x4]);
+		if (loginButton && signupButton) {
+			loginButton.addEventListener('click', function () {
+				checkWhichForm(loginCheck, signupCheck);
+			}, false);
+			signupButton.addEventListener('click', function () {
+				checkWhichForm(loginCheck, signupCheck);
+			}, false);
+		}
+	}
 
-  /** colour for question meta - views, likes and answered if count is greater than 0*/
-  const homeAnswered = document.getElementsByClassName('answer-count-dis');
-  const homeLiked = document.getElementsByClassName('likes-count-dis');
-  const homeViews = document.getElementsByClassName('views-count-dis');
+	return _loop2;
+}();
 
-  let countArr = [ homeAnswered,  homeLiked, homeViews];
-  const classCountArr = ['answered', 'liked', 'viewed'];
-  for( let y in countArr){
-    for( let x in countArr[y]){
-      let current = Number(countArr[y][x].textContent);
-      if( current > 0){
-          countArr[y][x].classList += " " +classCountArr[y];
-      }//if
-    }//for x
-  }//for y
-
-
-  /** mobile search configuration*/
-  const mobSearchIcon =document.getElementById('fa-search');
-  const searchForm = document.getElementsByClassName('mob-search-form');
-  const searchFormInput = document.getElementsByClassName('search-form-input');
-  mobSearchIcon.addEventListener('click', () => {
-    if( searchForm[0].style.display === "block"){
-      mobSearchIcon.style.color = "white";
-      searchForm[0].style.display = "none";
-      console.log(searchForm[0]);
-      console.log(searchForm[0].style.display);
-    }
-    else{
-      mobSearchIcon.style.color = "#f24d4d";
-      searchForm[0].style.display = "block";
-      console.log(searchForm[0]);
-      console.log(searchForm[0].style.display);
-    }
-  }, false);
-  searchFormInput[0].addEventListener('blur', ()=>{
-    searchForm[0].style.display = "none";
-    mobSearchIcon.style.color = "white";
-
-  }, false);
-
-
-
-  /** functions the display either login or sign up forms*/
-let loginCheckArr = ['login-form', 'side-login-form'];
-let signupCheckArr =['signup-form', 'side-signup-form'];
-let changeButtons = ['c-t-login' , 's-t-login'];
-let changeButtons2 =['c-t-signup', 's-t-signup'];
-const checkWhichForm = (login, signup) => {
-  if( login.style.display === 'none'){
-    signup.style.display ='none';
-    login.style.display = 'block';
-  }else{
-  signup.style.display ='block';
-  login.style.display = 'none';
-  }
-};//checkWhichForm
-for( let x in loginCheckArr){
-  let loginCheck = document.getElementById(loginCheckArr[x]);
-  let signupCheck = document.getElementById(signupCheckArr[x]);
-  let loginButton = document.getElementById(changeButtons[x]);
-  let signupButton = document.getElementById(changeButtons2[x]);
-  if(loginButton && signupButton){
-    loginButton.addEventListener('click',()=>{
-      checkWhichForm(loginCheck, signupCheck);
-    },false);
-    signupButton.addEventListener('click',()=>{
-      checkWhichForm(loginCheck, signupCheck);
-    },false);
-  }
-
+for (var _x4 in loginCheckArr) {
+	_loop2(_x4);
 }
--- js/signup-login.js
+++ js/signup-login.js
@@ -1,38 +1,120 @@
-const signupForm = document.getElementById('su-form');
+'use strict';
 
-const createUser  = (e) => {
-    e.preventDefault();
-    const newUser = {
-      firstName = signupForm.fname.value;
-      lastName = signupForm.lname.value;
-      password = signupForm.pwd.value;
-      email = signupForm.email.value;
-      username = signupForm.username.value;
-    };
-   
+var body = document.getElementsByTagName('body')[0];
+if (body.classList.contains('page-login-signup')) {
+  var signupForm = document.getElementById('su-form');
+  var createUser = function () {
+    function createUser(_e) {
+      _e.preventDefault();
+      var warning = document.getElementById('warning-message');
+      var newUser = new FormData();
+      newUser.append('firstName', signupForm.fname.value);
+      newUser.append('lastName', signupForm.lname.value);
+      newUser.append('occupation', signupForm.occupation.value);
+      newUser.append('password', signupForm.pwd.value);
+      newUser.append('email', signupForm.email.value);
+      newUser.append('username', signupForm.username.value);
+      newUser.append('profileImage', signupForm.profileImage.files[0]);
+      fetch('https://safe-inlet-99347.herokuapp.com/api/v2/auth/signup', {
+        method: 'POST',
+        body: newUser
+      }).then(function (res) {
+        return res.json();
+      }).then(function (data) {
+        var message = data.message;
+        if (message === 'user created') {
+          var _data$profile = data.profile,
+              username = _data$profile.username,
+              profileimage = _data$profile.profileimage;
+          var token = data.token;
 
+          localStorage.setItem('username', username);
+          localStorage.setItem('profileimage', profileimage);
+          localStorage.setItem('token', token);
+          window.location.href = './profile?username=' + String(username);
+          return;
+        }
+        return warning.textContent = message;
+      })['catch'](function (err) {
+        return err;
+      });
+    }
 
+    return createUser;
+  }();
+  signupForm.addEventListener('submit', createUser, false);
 
-    fetch('https://safe-inlet-99347.herokuapp.com/api/v2/auth/signup', {
+  var loginForm = document.getElementById('lg-form');
+  var loginUser = function () {
+    function loginUser(_e) {
+      _e.preventDefault();
+      var warning = document.getElementById('warning-message2');
+      var user = {
+        email: loginForm.email.value,
+        password: loginForm.pwd.value
+      };
+      fetch('https://safe-inlet-99347.herokuapp.com/api/v2/auth/login', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      }).then(function (res) {
+        return res.json();
+      }).then(function (data) {
+        var message = data.message;
+        if (message === "successfully logged in!") {
+          var _data$profile2 = data.profile,
+              username = _data$profile2.username,
+              profileimage = _data$profile2.profileimage;
+          var token = data.token;
+
+          localStorage.setItem('username', username);
+          localStorage.setItem('profileimage', profileimage);
+          localStorage.setItem('token', token);
+          window.location.href = './profile?username=' + String(username);
+          return;
+        }
+        return warning.textContent = 'username/password do not match';
+      })['catch'](function (error) {
+        return error;
+      });
+    }
+
+    return loginUser;
+  }();
+  loginForm.addEventListener('submit', loginUser, false);
+}
+
+var logout = document.getElementsByClassName('logout');
+var logoutUser = function () {
+  function logoutUser() {
+    var token = localStorage.getItem('token');
+    fetch('https://safe-inlet-99347.herokuapp.com/api/v2/auth/logout', {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(newUser)
-    }).then((res) => {
-        const data = res.json();
-        console.log(data);
-        const token = data.token;
-        const { fullName, username, email } = data.profile;
-        console.log(data.token);  
-        console.log(data.profile);
-        localStorage.setItem('token', token);
-        localStorage.setItem('fullName', fullName);
-        localStorage.setItem('username', username);        
-        window.location.href = './profile.html';
-      })
-      .catch((err) => console.log(err))
-  };
+      body: JSON.stringify({ token: token })
+    }).then(function (res) {
+      return res.json();
+    }).then(function () {
+      localStorage.clear();
+      if (currentPageTitle === 'Profile') {
+        window.location.href = '/';
+      } else {
+        document.location.reload();
+      }
+    })['catch'](function (error) {
+      return error;
+    });
+  }
 
-signupForm.addEventListener('submit', createUser, false);
+  return logoutUser;
+}();
+
+for (var x = 0; x < logout.length; x++) {
+  logout[x].addEventListener('click', logoutUser, false);
+}
